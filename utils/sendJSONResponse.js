4e298575800fed3e4f1bfea5f61e54d2d0fa4cf5
Gianella Xiomara Coronel Manchego <gia.ne12@hotmail.com>
2026-01-21 12:29:29 -0500

    feat: add code for CORS

--- utils/sendJSONResponse.js
+++ utils/sendJSONResponse.js
@@ -1,5 +1,9 @@
 export const sendJSONResponse = (res, statusCode, payload) => {
   res.setHeader("Content-Type", "application/json");
+  /* This is to avoid CORS error, to make everyone use this API with GET method */
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+  /* *** */
   res.statusCode = statusCode;
   res.end(JSON.stringify(payload));
 };
