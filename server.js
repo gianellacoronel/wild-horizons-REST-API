163c941f2849551ebd85da899c9ba186a6a699b0
Gianella Xiomara Coronel Manchego <gia.ne12@hotmail.com>
2026-01-21 09:55:18 -0500

    feat: creating a basic server using a specific port

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
 import http from "node:http";
+
+const PORT = 8000;
+
+const server = http.createServer((req, res) => {
+  res.end("Hello from the server!");
+});
+
+server.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
